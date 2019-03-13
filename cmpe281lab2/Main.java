3cc39bd2d9cbda2288d2dee2063bb94f0e2a853e
anushreemenon <47263849+anushreemenon@users.noreply.github.com>
2019-03-12 22:53:02 -0700

    Add files via upload

---
+++ cmpe281lab2/Main.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class Lab281 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, This is my CMPE 281 Lab2 assignment!");
+    }
+}
